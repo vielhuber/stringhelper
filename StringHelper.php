6a34251e397cf6d767079223508b7c47764a288f
David Vielhuber <david@vielhuber.de>
2017-09-10 21:37:33 +0200

    refactored and added new function

--- StringHelper.php
+++ StringHelper.php
@@ -1,16 +1,20 @@
 <?php
-// dynamic function to check if multiple variables exists
-function __mx() {
-    for($i = 0 ; $i < func_num_args(); $i++) {
+// check if multiple variables exists
+function __mx()
+{
+    for($i = 0 ; $i < func_num_args(); $i++)
+    {
         $arg = func_get_arg($i);
         if( @__nx($arg) ) { return false; }
     }
     return true;
 }
 
-// dynamic function to check if at least one variables exists
-function __ox() {
-    for($i = 0 ; $i < func_num_args(); $i++) {
+// check if at least one variable exists
+function __ox()
+{
+    for($i = 0 ; $i < func_num_args(); $i++)
+    {
         $arg = func_get_arg($i);
         if( @__x($arg) ) { return true; }
     }
@@ -18,23 +22,29 @@ function __ox() {
 }
 
 // return empty model
-class __ignore_helper {
-    public function __call($name, $arguments) {
+class __ignore_helper
+{
+    public function __call($name, $arguments)
+    {
         return new __ignore_helper();
     }
-	public function __toString() {
+	public function __toString()
+	{
 		return '';
 	}
-    public function count() {
+    public function count()
+    {
         return 0;
     }
 }
-function __empty() {
+function __empty()
+{
     return new __ignore_helper();
 }
 
-// if string exists
-function __x($input) {
+// check if string exists
+function __x($input)
+{
     if( $input === null || $input === '' || trim($input) === '' || (is_array($input) && empty($input)) || (is_object($input) && empty((array)$input)) ) { return false; }
     if( is_array($input) && count($input) === 1 && array_values($input)[0] === '' ) { return false; }
     if($input instanceof Illuminate\Database\Eloquent\Relations\BelongsTo && $input->count() === 0 ) { return false; }
@@ -44,16 +54,20 @@ function __x($input) {
     return true;
 }
 
-// if string does not exist
-function __nx($var) {
+// check if string does not exist
+function __nx($var)
+{
     return !@__x($var);
 }
 
 // check if array has minimum one variable that exists
-function __aox($var) {
+function __aox($var)
+{
     if( !is_array($var) ) { return false; }
-    foreach($var as $key=>$value) {
-        if( @__x($value) ) {
+    foreach($var as $key=>$value)
+    {
+        if( @__x($value) )
+        {
             return true;
         }
     }
@@ -61,10 +75,13 @@ function __aox($var) {
 }
 
 // check if every variable in array exists
-function __amx($var) {
+function __amx($var)
+{
     if( !is_array($var) ) { return false; }
-    foreach($var as $key=>$value) {
-        if( @__nx($value) ) {
+    foreach($var as $key=>$value)
+    {
+        if( @__nx($value) )
+        {
             return false;
         }
     }
@@ -73,21 +90,35 @@ function __amx($var) {
 
 
 // return first existing value, otherwise null
-function __f(...$args) {
-    foreach($args as $arg) {
+function __f(...$args)
+{
+    foreach($args as $arg)
+    {
         if( @__x($arg) ) { return trim($arg); }
     }
     return null;
 }
 
 // if first value exists, return second value, otherwise third
-function __xe($var,$return,$fallback = null) {
+function __xe($var,$return,$fallback = null)
+{
     if( @__x($var) ) { return $return; }
     return $fallback;
 }
 
+// check equality of two values (weak check)
+function __eq($a, $b)
+{
+	if( @__nx($a) && @__nx($b) { return true; }
+	if( @__nx($a) && @__x($b) { return false; }
+	if( @__x($a) && @__nx($b) { return false; }
+	if( $a == $b ) { return true; }
+	return false;
+}
+
 // debug output
-function __d($data = array(), $die = false) {
+function __d($data = [], $die = false)
+{
     echo '<pre>';
     print_r($data);
     echo '</pre>';
@@ -95,41 +126,50 @@ function __d($data = array(), $die = false) {
 }
 
 // get nth element of concatenized array
-function __expl($separator = " ", $array = array(), $pos = 0) {
+function __expl($separator = ' ', $array = [], $pos = 0)
+{
     return current(array_slice(explode($separator,$array), $pos, 1));
 }
 
 // post/redirect/get-pattern
-function __prg($url = null) {
-    if($url == null) {
+function __prg($url = null)
+{
+    if($url == null)
+    {
         $url = (@$_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
-        $url .= $_SERVER['HTTP_HOST'].strtok($_SERVER["REQUEST_URI"],'?');
-        if(@__x($_GET["page_id"])) { $url .= '?page_id='.$_GET["page_id"]; }
+        $url .= $_SERVER['HTTP_HOST'].strtok($_SERVER['REQUEST_URI'],'?');
+        if(@__x($_GET['page_id'])) { $url .= '?page_id='.$_GET['page_id']; }
     }
-    header("Location: " . $url);
+    header('Location: '.$url);
     die();
 }
 
 // returns null if date is invalid, otherwise formatted date
-function __date($date, $format = 'Y-m-d') {
+function __date($date, $format = 'Y-m-d')
+{
     if( !@__validate_date($date) ) { return null; }
     return date($format,strtotime($date));
 }
 
 // checks if a date is valid (in english and german format)
-function __validate_date($date) {
+function __validate_date($date)
+{
     if( @__nx($date) ) { return false; }
-    $date = explode(" ",$date)[0];
-    if( substr_count($date,"-") == 2 ) {
+    $date = explode(' ',$date)[0];
+    if( substr_count($date,'-') == 2 )
+    {
         $date = explode('-', $date);
-        if(checkdate($date[1], $date[2], $date[0])) {
+        if(checkdate($date[1], $date[2], $date[0]))
+        {
             if( $date[0] >= 2037 ) { return false; } // prevent 32-bit problem
             return true;
         }
     }
-    else if( substr_count($date,".") == 2 ) {
+    else if( substr_count($date,'.') == 2 )
+    {
         $date = explode('.', $date);
-        if(checkdate($date[1], $date[0], $date[2])) {
+        if(checkdate($date[1], $date[0], $date[2]))
+        {
             if( $date[2] >= 2037 ) { return false; } // prevent 32-bit problem
             return true;
         }
@@ -138,26 +178,32 @@ function __validate_date($date) {
 }
 
 // check if email is valid
-function __validate_email($email) {
+function __validate_email($email)
+{
     if(@__nx($email)) { return false; }
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
       return true;
     }
     return false;
 }
 
 // outputs a valid formatted value for input datetime-local
-function __datetime($datetime) {
+function __datetime($datetime)
+{
     if( @__nx($datetime) ) { return null; }
-    return date('Y-m-d', strtotime($datetime))."T".date('H:i', strtotime($datetime));
+    return date('Y-m-d', strtotime($datetime)).'T'.date('H:i', strtotime($datetime));
 }
 
 
 // flatten multidimensional array (keys)
-function __flatten_keys($array) {
+function __flatten_keys($array)
+{
     $return = [];
-    foreach($array as $key=>$value) {
-        if(is_array($value)) {
+    foreach($array as $key=>$value)
+    {
+        if(is_array($value))
+        {
             $return = array_merge($return, __flatten_keys($value));
         }
         $return[] = $key;
@@ -166,14 +212,16 @@ function __flatten_keys($array) {
 }
 
 // flatten multidimensional array (values)
-function __flatten_values($array) {
+function __flatten_values($array)
+{
     $return = [];
     array_walk_recursive($array, function($a) use (&$return) { $return[] = $a; });
     return $return;
 }
 
 // string to slug (sanitize string)
-function __slug($string) {
+function __slug($string)
+{
 	// replace non letter or digits by -
 	$string = preg_replace('~[^\pL\d]+~u', '-', $string);
 	// transliterate
@@ -191,56 +239,67 @@ function __slug($string) {
 }
 
 // check if key is first key in foreach loop
-function __fkey($array__key,$array) {
-	if( array_keys($array)[0] === $array__key ) {
+function __fkey($array__key,$array)
+{
+	if( array_keys($array)[0] === $array__key )
+	{
 		return true;
 	}
 	return false;
 }
 
 // check if key is last key in foreach loop
-function __lkey($array__key,$array) {
-	if( array_keys($array)[count(array_keys($array))-1] === $array__key ) {
+function __lkey($array__key,$array)
+{
+	if( array_keys($array)[count(array_keys($array))-1] === $array__key )
+	{
 		return true;
 	}
 	return false;
 }
 
 // get last item of array
-function __last($array) {
+function __last($array)
+{
     return array_values(array_slice($array, -1))[0];
 }
 
 // get first item of array
-function __first($array) {
+function __first($array)
+{
     return $array[0];
 }
 
 // get random element from array
-function __rand($array) {
+function __rand($array)
+{
     return $array[array_rand($array)];
 }
 
 // generate random string
-function __random_string($length = 8, $chars = null) {
+function __random_string($length = 8, $chars = null)
+{
 	if( $chars === null ) { $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; }
     $chars_length = strlen($chars);
     $random_string = '';
-    for($i = 0; $i < $length; $i++) {
+    for($i = 0; $i < $length; $i++)
+    {
         $random_string .= $chars[mt_rand(0, $chars_length-1)];
     }
     return $random_string;
 }
 
 // swap values
-function __swap(&$x, &$y) {
+function __swap(&$x, &$y)
+{
     $tmp = $x;
     $x = $y;
     $y = $tmp;
 }
 
 // extract from string
-function __extract($string, $begin, $end) {
+function __extract($string, $begin, $end)
+{
 	$pos1 = strpos($string,$begin)+strlen($begin);
 	if($pos1 === false) { return false; }
 	$pos2 = strpos($string, $end, $pos1);
@@ -249,23 +308,27 @@ function __extract($string, $begin, $end) {
 }
 
 // strip string
-function __strip($string, $length = 50, $dots = '...') {
+function __strip($string, $length = 50, $dots = '...')
+{
 	if( mb_strlen($string) <= $length ) { return $string; }
 	return rtrim(mb_substr($string, 0, $length)).$dots;
 }
 
 // strip non numeric (all except 0-9, ., .)
-function __strip_nonnumeric($string) {
+function __strip_nonnumeric($string)
+{
 	return preg_replace('/[^0-9,.]/', '', $string);	 
 }
 
 // strip non digit (all except 0-9)
-function __strip_nondigit($string) {
+function __strip_nondigit($string)
+{
 	return preg_replace('/[^0-9]/', '', $string);
 }
 
 // checks if variable is an integer (works also for big integers)
-function __is_integer($input) {
+function __is_integer($input)
+{
     if( @__nx($input) ) { return false; }
     if( is_int($input) ) { return true; }
     if( is_numeric($input) && ($input != (string)(float)$input) ) { return true; }
