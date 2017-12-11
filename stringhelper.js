6d4f390f6e732c9f83a6581a71504f1d4e9df4d5
David Vielhuber <david@vielhuber.de>
2017-12-11 16:13:48 +0100

    added __get to js

--- stringhelper.js
+++ stringhelper.js
@@ -42,7 +42,7 @@ function __get(variable) {
     for(var i=0; i < vars.length; i++)
     {
         var pair = vars[i].split('=');
-        if(pair[0] == variable)
+        if(pair[0] == variable && __x(pair[1]))
         {
             return pair[1];
         }
